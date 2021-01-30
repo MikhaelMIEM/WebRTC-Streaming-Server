95ff922d3cc78fe02982f0bd9a12cfa6f0cbe2c4
MikhaelMIEM <mvnikolenko@edu.hse.ru>
2021-01-30 21:55:26 +0300

    fix

--- media_server/WebRTCStreamingServer.py
+++ media_server/WebRTCStreamingServer.py
@@ -229,7 +229,7 @@ async def classify(request):
         # img_path = get_file(str(time()), origin=img_url)
         # img = image.load_img(img_path, target_size=(224, 224))
         x = image.img_to_array(im)
-        x = keras.preprocessing.image.img_to_array(img)
+        # x = keras.preprocessing.image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         preds = model.predict(x)
